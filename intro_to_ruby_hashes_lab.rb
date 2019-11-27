724f3243ba0f72739f85349956a4fab491f53eb1
Alexandra Pestruyeva <apestruy@gmail.com>
2019-11-27 04:24:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,13 +5,16 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-    :railroads => {:pieces => 4},
-                  {:rent_in_dollars => {} }
-    } 
+     :railroads => {
+      :pieces => 4
+    } }
 end
 
 def monopoly_with_third_tier
    monopoly = {
+     :railroads => {:pieces => 4},
+                  {:rent_in_dollars => {} }
+    }
     :railroads => {
       :pieces => 4
     } }
